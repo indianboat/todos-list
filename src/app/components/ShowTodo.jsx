c3a1cab690ef1d28f84b0edb16ad728ebaedd984
unknown <iampankaj852@gmail.com>
2023-06-15 11:19:01 +0530

    chip added for done & pending

--- src/app/components/ShowTodo.jsx
+++ src/app/components/ShowTodo.jsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { Delete, TickSquare } from "react-iconly";
+import { Calendar, Delete, TickSquare } from "react-iconly";
 import { deleteTask } from "./DeleteTodo";
 import { updateTask } from "./UpdateTodo";
 import { useEffect, useState } from "react";
 import { TbSortAscending, TbSortDescending } from "react-icons/tb";
 
 const ShowTodo = ({ todos }) => {
-
   const [sortingOrder, setSortingOrder] = useState("lo");
 
   useEffect(() => {
@@ -21,11 +20,33 @@ const ShowTodo = ({ todos }) => {
     localStorage.setItem("sortingOrder", sortingOrder);
   }, [sortingOrder]);
 
+
+  // let now = new Date().toDateString();
+  // let then = todos[0].createdAt.toDateString();
+  // console.log(now == then);
+
+
   return (
     <>
       <div className="my-6 gap-y-3 flex flex-col">
-        <div className="flex">
-          <button className="p-2 rounded-full text-slate-600 bg-slate-200 dark:bg-slate-950" onClick={() => sortingOrder == "lo" ? setSortingOrder("ol") : setSortingOrder("lo")}>{sortingOrder === "lo" ? <TbSortAscending title="Latest to Old" size={24}/> : <TbSortDescending title="Old to Latest" size={24}/>}</button>
+        <div className="flex justify-between">
+          <button
+            className="p-2 rounded-full text-slate-600 bg-slate-200 dark:bg-slate-950"
+            onClick={() =>
+              sortingOrder == "lo"
+                ? setSortingOrder("ol")
+                : setSortingOrder("lo")
+            }
+          >
+            {sortingOrder === "lo" ? (
+              <TbSortAscending title="Latest to Old" size={24} />
+            ) : (
+              <TbSortDescending title="Old to Latest" size={24} />
+            )}
+          </button>
+          <button className="p-2 rounded-full text-slate-600 bg-slate-200 dark:bg-slate-950">
+            <Calendar title="Latest to Old" size={24}/>
+          </button>
         </div>
 
         {sortingOrder === "lo"
@@ -66,12 +87,18 @@ const ShowTodo = ({ todos }) => {
                     </div>
                     <div className="flex justify-between mt-2">
                       <span className="text-xs px-2 text-gray-400 italic">
-                        {todo?.createdAt.toDateString()}{" "}
+                        {todo?.createdAt.toDateString()}
                         {todo?.createdAt.toLocaleTimeString().toLowerCase()}
                       </span>
-                      <span className="text-xs px-2 py-1 rounded-xl text-gray-700 italic bg-slate-100 dark:bg-slate-200 ">
-                        office work
-                      </span>
+                      {todo?.isCompleted === true ? (
+                        <span className="text-xs px-2 py-1 rounded-xl text-green-700 dark:text-green-400 italic bg-green-200 dark:bg-green-900">
+                          Done
+                        </span>
+                      ) : (
+                        <span className="text-xs px-2 py-1 rounded-xl text-yellow-700 dark:text-yellow-400 italic bg-yellow-100 dark:bg-yellow-900">
+                          Pending
+                        </span>
+                      )}
                     </div>
                   </div>
                 );
@@ -116,9 +143,15 @@ const ShowTodo = ({ todos }) => {
                       {todo?.createdAt.toDateString()}{" "}
                       {todo?.createdAt.toLocaleTimeString().toLowerCase()}
                     </span>
-                    <span className="text-xs px-2 py-1 rounded-xl text-gray-700 italic bg-slate-100 dark:bg-slate-200 ">
-                      office work
-                    </span>
+                    {todo?.isCompleted === true ? (
+                      <span className="text-xs px-2 py-1 rounded-xl text-green-700 italic bg-green-100 dark:bg-green-200 ">
+                        Done
+                      </span>
+                    ) : (
+                      <span className="text-xs px-2 py-1 rounded-xl text-gray-700 italic bg-slate-100 dark:bg-slate-200 ">
+                        Pending
+                      </span>
+                    )}
                   </div>
                 </div>
               );
